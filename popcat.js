8d4a6717781b9bdada1f63ef996d6721a08bbc02
atitorn <tumatitorn@gmail.com>
2021-08-15 16:25:18 +0700

    Update popcat.js

--- popcat.js
+++ popcat.js
@@ -8,9 +8,9 @@ var rapidClick = new KeyboardEvent(
 var clck = 0;
 var interval = setInterval(function () {
   clck += 1;
-  if (clck === 1000) {
+  if (clck === 1000) { // increase number here to increase click no.
     clearInterval(interval);
   } else {
     document.dispatchEvent(rapidClick);
   }
-}, 100);
+}, 100);  // increase click speed by reducing this number
